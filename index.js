363d120f08ff355dd12363e2841f2afad70984be
gstmoniz <gustavo.moniz.ribeiro@gmail.com>
2022-11-15 20:02:44 -0300

    refatoracao array 1

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ buttonHamburguer.addEventListener('click', () => {
     menuEsquerda.classList.toggle('menu-esquerda--ativo')
 })
 
-/*futuramente o código da adição das classes será implementado!*/
-
 const linkEsquerda1 = document.querySelector('.menu-esquerda-link-1');
 const linkEsquerda2 = document.querySelector('.menu-esquerda-link-2');
 const linkEsquerda3 = document.querySelector('.menu-esquerda-link-3');
@@ -14,26 +12,11 @@ const linkEsquerda4 = document.querySelector('.menu-esquerda-link-4');
 const linkEsquerda5 = document.querySelector('.menu-esquerda-link-5');
 const linkEsquerda6 = document.querySelector('.menu-esquerda-link-6');
 
-linkEsquerda1.addEventListener('click', () => {
-    linkEsquerda1.classList.toggle('menu-esquerda-link--ativo')
-})
+/*refatoracao array 1*/
 
-linkEsquerda2.addEventListener('click', () => {
-    linkEsquerda2.classList.toggle('menu-esquerda-link--ativo')
-})
+const listaLinks = [linkEsquerda1,linkEsquerda2,linkEsquerda3,linkEsquerda4,linkEsquerda5,linkEsquerda6]
 
-linkEsquerda3.addEventListener('click', () => {
-    linkEsquerda3.classList.toggle('menu-esquerda-link--ativo')
-})
-
-linkEsquerda4.addEventListener('click', () => {
-    linkEsquerda4.classList.toggle('menu-esquerda-link--ativo')
-})
-
-linkEsquerda5.addEventListener('click', () => {
-    linkEsquerda5.classList.toggle('menu-esquerda-link--ativo')
-})
-
-linkEsquerda6.addEventListener('click', () => {
-    linkEsquerda6.classList.toggle('menu-esquerda-link--ativo')
-})
+listaLinks.forEach(element => {
+    element.addEventListener('click', () => {
+        element.classList.toggle('menu-esquerda-link--ativo')})    
+});
