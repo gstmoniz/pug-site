485817800e19e2a29ce928d752f398777aff18a5
gstmoniz <gustavo.moniz.ribeiro@gmail.com>
2022-11-15 21:00:02 -0300

    refatoracao array 2

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ buttonHamburguer.addEventListener('click', () => {
     menuEsquerda.classList.toggle('menu-esquerda--ativo')
 })
 
-const linkEsquerda1 = document.querySelector('.menu-esquerda-link-1');
-const linkEsquerda2 = document.querySelector('.menu-esquerda-link-2');
-const linkEsquerda3 = document.querySelector('.menu-esquerda-link-3');
-const linkEsquerda4 = document.querySelector('.menu-esquerda-link-4');
-const linkEsquerda5 = document.querySelector('.menu-esquerda-link-5');
-const linkEsquerda6 = document.querySelector('.menu-esquerda-link-6');
+/*refatoracao array 2*/
+
+var k = 'linkEsquerda';
+var i = 0;
+
+for(i = 1; i < 7; i++) {
+    eval('var ' + k + i + '= ' + 'document.querySelector(\'.menu-esquerda-link-' + i + '\')' + ';');
+}
+
+// const linkEsquerda1 = document.querySelector('.menu-esquerda-link-1');
+// const linkEsquerda2 = document.querySelector('.menu-esquerda-link-2');
+// const linkEsquerda3 = document.querySelector('.menu-esquerda-link-3');
+// const linkEsquerda4 = document.querySelector('.menu-esquerda-link-4');
+// const linkEsquerda5 = document.querySelector('.menu-esquerda-link-5');
+// const linkEsquerda6 = document.querySelector('.menu-esquerda-link-6');
 
 /*refatoracao array 1*/
 
